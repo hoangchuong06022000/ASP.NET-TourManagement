55d788dd035860ddf120d9e40d80252b587f8709
Văn Hoàng Chương <hoangchuong06022000@gmail.com>
2021-12-18 17:06:57 +0700

    Re-Add Components

---
+++ Model/DAL/DAL_ThongKe.cs
@@ -0,0 +1,52 @@
+﻿using Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAL
+{
+    public class DAL_ThongKe
+    {
+        private QLDLEntity db = new QLDLEntity();
+        public int SoTour()
+        {
+            return db.TOURDULICHes.Count();
+        }
+
+        public int SoDoan()
+        {
+            return db.DOANDULICHes.Count();
+        }
+
+        public int SoKhachHang()
+        {
+            return db.KHACHes.Count();
+        }
+
+        public int SoKhachTheoDoan(string maDoan)
+        {
+            CHITIETDOAN ctDoan = (CHITIETDOAN) db.CHITIETDOANs.Where(a => a.MADOAN == maDoan);
+            return db.KHACHes.Where(a => a.MAKH == ctDoan.MAKH).Count();
+        }
+
+        public int SoLanDiTourNV(string maNV)
+        {
+            NHANVIEN nv = (NHANVIEN)db.NHANVIENs.Where(a => a.MANV == maNV);
+            return db.PHANBONHANVIENs.Where(a => a.MANV == nv.MANV).Count();
+        }
+
+        public double DoanhThuMoiDoan(string maDoan)
+        {
+            DOANDULICH doan = (DOANDULICH)db.DOANDULICHes.Where(a => a.MADOAN == maDoan);
+            return doan.DOANHTHU;
+        }
+
+        public double ChiPhiMoiDoan(string maDoan)
+        {
+            CHIPHI chiPhi = (CHIPHI)db.CHIPHIs.Where(a => a.MADOAN == maDoan);
+            return chiPhi.SOTIEN.GetValueOrDefault();
+        }
+    }
+}
--- Nhom05_QLDL/Controllers/StatiticController.cs
+++ Nhom05_QLDL/Controllers/StatiticController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model.Model;
+using Model.DAL;
 
 namespace Nhom05_QLDL.Controllers
 {
     public class StatiticController : Controller
     {
+        private DAL_ThongKe thongKe = new DAL_ThongKe();
         // GET: Statitic
         public ActionResult Statitic()
         {
+            ViewBag.SoTour = thongKe.SoTour();
+            ViewBag.SoDoan = thongKe.SoDoan();
+            ViewBag.SoKhachHang = thongKe.SoKhachHang();
+            ViewBag.SoKhachTheoDoan = thongKe.SoKhachTheoDoan(null);
             return View();
         }
     }
